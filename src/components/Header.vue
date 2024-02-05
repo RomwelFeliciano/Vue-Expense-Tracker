b2718d1e791ae4dceaf3a7a94023c8c07255b948
elciddelapaz <127919521+RomwelFeliciano@users.noreply.github.com>
2024-02-05 15:31:30 +0800

    first commit

---
+++ src/components/Header.vue
@@ -0,0 +1,5 @@
+<template>
+    <header>
+        <h2>Expense Tracker</h2>
+    </header>
+</template>
---
+++ src/components/TransactionList.vue
@@ -0,0 +1,29 @@
+<template>
+    <h3>History</h3>
+    <ul id="list" class="list" :class="transactions.length > 3 ? 'overflow' : null">
+        <li v-for="transaction in transactions" :key="transaction.id" :class="transaction.amount < 0 ? 'minus' : 'plus'">
+            {{
+                transaction.text }} <span>₱{{ Intl.NumberFormat().format(transaction.amount)
+    }}</span><button @click="deleteTransaction(transaction.id)" class="delete-btn">x</button></li>
+    </ul>
+</template>
+
+<script setup>
+import { defineProps } from 'vue';
+
+const emit = defineEmits(['transactionDeleted'])
+
+const props = defineProps({
+    transactions: {
+        type: Array,
+        required: true
+    }
+})
+
+const deleteTransaction = (id) => {
+    emit('transactionDeleted', id)
+}
+
+</script>
+
+<style lang="scss" scoped></style>
